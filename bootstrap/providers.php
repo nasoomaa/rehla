e7fd8a7e05a9c5e3f9901093f8eca6f54b4372b4
nesma fahmi <nasoomahmam@gmail.com>
2026-08-22 23:12:54 +0000

    chore(m0): register providers in topological order

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,22 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    Rehla\Core\Providers\CoreServiceProvider::class,
+    Rehla\Datagrid\Providers\DatagridServiceProvider::class,
+    Rehla\Rule\Providers\RuleServiceProvider::class,
+    Rehla\Media\Providers\MediaServiceProvider::class,
+    Rehla\ImageCache\Providers\ImageCacheServiceProvider::class,
+    Rehla\Customers\Providers\CustomersServiceProvider::class,
+    Rehla\AdminUsers\Providers\AdminUsersServiceProvider::class,
+    Rehla\Catalog\Providers\CatalogServiceProvider::class,
+    Rehla\CartRule\Providers\CartRuleServiceProvider::class,
+    Rehla\Sales\Providers\SalesServiceProvider::class,
+    Rehla\Payment\Providers\PaymentServiceProvider::class,
+    Rehla\Checkout\Providers\CheckoutServiceProvider::class,
+    Rehla\Applications\Providers\ApplicationsServiceProvider::class,
+    Rehla\Notifications\Providers\NotificationsServiceProvider::class,
+    Rehla\AuditLog\Providers\AuditLogServiceProvider::class,
+    Rehla\Dashboard\Providers\DashboardServiceProvider::class,
+    Rehla\Api\Providers\ApiServiceProvider::class,
 ];
